cde0ed9236d54c7949f6946265971bdc98ab094b
Debora <debora.carvalho589@gmail.com>
2023-09-18 19:13:10 -0300

    Adicionar arquivos de projeto.

---
+++ Prova_POO2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prova_POO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
