f2bd710b9a1f8063299b5f98613dbff95b96895f
Debora <debora.carvalho589@gmail.com>
2023-09-18 21:49:09 -0300

    entrega prova

---
+++ ex02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ex02
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AlgumaClasse Objeto = new();
+            Objeto.CampoX = 1;
+            Reflections.RetornaCamposPublicos(Objeto);
+        }
+
+        public class AlgumaClasse
+        {
+            public int CampoX;
+            private int CampoY;
+            public int Propriedade1 { get; set; }
+            private int Propriedade2 { get; set; }
+
+            public AlgumaClasse() { }
+            public string Metodo1()
+            {
+                return "Método1 chamado";
+            }
+
+            private string Metodo2()
+            {
+                return "Method2 chamado";
+            }
+        }
+        public static class Reflections
+        {
+            public static void RetornaCamposPublicos(object obj)
+            {
+                
+                    var tipo = obj.GetType().GetFields();
+
+            }
+
+        }
+
+    }      
+}
---
+++ ex03/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ex03
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public class ObjetoComposto<T>
+        {
+            public T Chave { get; set; }
+            public T Valor { get; set;}
+
+            public string Exibir()
+            {
+                return $"Chave: {Chave} | Valor: {Valor}";
+            }
+        }
+
+    }
+}
